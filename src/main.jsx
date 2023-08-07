b081c4178a4ecb950ba9f20f2179ada8d7ec4bf6
BullFrog <camroth2002@gmail.com>
2023-08-06 20:24:32 -0600

    finished main and root, all routes should work they just need content

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,37 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page";
 import "./index.css";
+import Login from "./components/login";
+import Logout from "./components/logout";
 import Root from "./routes/root";
+import Home from "./components/Home";
 import MyRoutines from "./components/MyRoutines";
+import Register from "./components/Register";
 import Activities from "./components/Activities";
-import Users from "./components/users";
-// import Login from "./components/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    // errorElement: <ErrorPage />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
         element: <Login />,
       },
       {
-        path: "/users",
-        element: <Users />,
+        path: "/Logout",
+        element: <Logout />,
+      },
+      {
+        path: "/Register",
+        element: <Register />,
+      },
+      {
+        path: "/Home",
+        element: <Home />,
       },
       {
         path: "/MyRoutines",
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -8,35 +8,20 @@ export default function Root() {
         <div className="container mx-auto">
           <div className="flex items-center justify-between px-10 py-8 text-3xl">
             <NavLink
-              to={`/HomePage`}
+              to={`/Home`}
               className={({ isActive, isPending }) =>
                 isActive ? "active" : isPending ? "pending" : ""
               }
             >
               Fitness Tracker
             </NavLink>
-            <button
-              className="text-white focus:outline-none focus:text-white lg:hidden"
-              type="button"
-              aria-label="Toggle navigation"
-            >
-              <svg
-                className="h-6 w-6 fill-current"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  className="heroicon-ui"
-                  d="M20 12H4M20 6H4M20 18H4"
-                ></path>
-              </svg>
-            </button>
+
             <div className=" flex items-center">
               <div className="mr-10">
                 <Link to={"/Home"} className="text-white text-2xl mr-10">
                   Home
                 </Link>
-                <Link to={"/MyRoutines"} className="text-white text-2xl mr-10">
+                <Link to={"/MyRoutines"} className="text-black text-2xl mr-10">
                   MyRoutines
                 </Link>
                 <Link to={"/Activities"} className="text-white text-2xl">
